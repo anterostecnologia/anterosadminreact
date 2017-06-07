0de7379b30b66f414d1511fe607823c596d70223
eduardogreco <eduardogreco93@gmail.com>
2017-06-07 17:47:36 -0300

    Exemplos de label e alteracoes componente

--- src/app/components/AnterosLabel.jsx
+++ src/app/components/AnterosLabel.jsx
@@ -1,30 +1,48 @@
 import React, { Component } from 'react';
 
-
-
 export default class AnterosLabel extends Component {
+
+    constructor(props) {
+        super(props);
+    }
     render() {
         let className = "label-custom";
-        if (this.props.primary){
-            className += " label-primary";
-        } else if (this.props.success){
-            className += " label-success";
-        } else if (this.props.info){
-            className += " label-info";            
-        } else if (this.props.warning){
-            className += " label-warning";            
-        } else if (this.props.danger){
-            className += " label-danger";            
+        if (this.props.primary) {
+            className += (this.props.outline ? " label-outline-primary" : " label-primary");
+        } else if (this.props.success) {
+            className += (this.props.outline ? " label-outline-success" : " label-success");
+        } else if (this.props.info) {
+            className += (this.props.outline ? " label-outline-info" : " label-info");
+        } else if (this.props.warning) {
+            className += (this.props.outline ? " label-outline-warning" : " label-warning");
+        } else if (this.props.danger) {
+            className += (this.props.outline ? " label-outline-danger" : " label-danger");
+        } else if (this.props.dark) {
+            className += (this.props.outline ? " label-outline-dark" : " label-dark");
         } else {
-            className += " label-default";
+            className += (this.props.outline ? " label-outline-default" : " label-default");
         }
-        if (this.props.pillFormat){
+        if (this.props.pillFormat) {
             className += " label-pill";
         }
+
+
 
         return (<span className={className}>{this.props.caption}</span>);
     }
 }
+
+AnterosLabel.propTypes = {
+    primary: React.PropTypes.bool,
+    success: React.PropTypes.bool,
+    info: React.PropTypes.bool,
+    warning: React.PropTypes.bool,
+    danger: React.PropTypes.bool,
+    dark: React.PropTypes.bool,
+    pillFormat: React.PropTypes.bool,
+    outline: React.PropTypes.bool,
+    caption: React.PropTypes.string,
+};
 
 
 
--- src/app/containers/LabelsView.jsx
+++ src/app/containers/LabelsView.jsx
@@ -7,18 +7,41 @@ export default class LabelView extends Component {
         return (<AnterosCard caption="Labels">
 
             <div className="row">
-                <div className="col-6">
-                    <AnterosCard caption="Default Labels">
+                <div className="col-4">
+                    <AnterosCard caption="Default">
+                        <AnterosLabel caption="Default" />
+                        <AnterosLabel primary caption="Primary" />
+                        <AnterosLabel success caption="Success" />
+                        <AnterosLabel info caption="Info" />
+                        <AnterosLabel warning caption="Warning" />
+                        <AnterosLabel danger caption="Danger" />
+                        <AnterosLabel dark caption="Dark" />
                     </AnterosCard>
                 </div>
-                <div className="col-6">
-                    <AnterosCard caption="Sizing">
-
+                <div className="col-4">
+                    <AnterosCard caption="Pill">
+                        <AnterosLabel pillFormat caption="Default" />
+                        <AnterosLabel pillFormat primary caption="Primary" />
+                        <AnterosLabel pillFormat success caption="Success" />
+                        <AnterosLabel pillFormat info caption="Info" />
+                        <AnterosLabel pillFormat warning caption="Warning" />
+                        <AnterosLabel pillFormat danger caption="Danger" />
+                        <AnterosLabel pillFormat dark caption="Dark" />
                     </AnterosCard>
                 </div>
-
-                <div className="col-6">
-                    <AnterosCard caption="Pill Labels">
+                <div className="col-4">
+                    <AnterosCard caption="Outline">
+                        <AnterosLabel outline caption="Default" />
+                        <AnterosLabel outline primary caption="Primary" />
+                        <AnterosLabel outline success caption="Success" />
+                        <AnterosLabel outline info caption="Info" />
+                        <AnterosLabel outline warning caption="Warning" />
+                        <AnterosLabel outline danger caption="Danger" />
+                        <AnterosLabel outline dark caption="Dark" />
+                    </AnterosCard>
+                </div>
+                <div className="col-4">
+                    <AnterosCard caption="Sizes">
 
                     </AnterosCard>
                 </div>
