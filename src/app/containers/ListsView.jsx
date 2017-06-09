cb927c31e78568c80bf6991b0f9e595c6b439d1f
Eduardo Albertini <ALBERTINI-MAC@MacBook-Pro-de-Eduardo.local>
2017-06-09 08:18:07 -0300

    Continuando implementação listsView

--- src/app/containers/ListsView.jsx
+++ src/app/containers/ListsView.jsx
@@ -7,6 +7,115 @@ export default class ListsView extends Component {
             <div>
                 <AnterosCard caption="List">
 
+                    <div className="row">
+                        <div className="col-md-4">
+                            <AnterosCard caption="Basic">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+
+                        <div className="col-md-4">
+                            <AnterosCard caption="With icon">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+
+                        <div className="col-md-4">
+                            <AnterosCard caption="With badges">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-4">
+                            <AnterosCard caption="Button items">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+
+                        <div className="col-md-4">
+                            <AnterosCard caption="Disabled items">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+
+                        <div className="col-md-4">
+                            <AnterosCard caption="Disabled items 2">
+                                <section className="section">
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <AnterosCard caption="Custom">
+                                <section className="section">
+
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <AnterosCard caption="Custom 1">
+                                                <section className="section">
+
+                                                </section>
+                                            </AnterosCard>
+                                        </div>
+
+                                        <div className="col-md-6">
+                                            <AnterosCard caption="Custom 2">
+                                                <section className="section">
+
+                                                </section>
+                                            </AnterosCard>
+                                        </div>
+                                    </div>
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <AnterosCard caption="Custom">
+                                <section className="section">
+
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <AnterosCard caption="With dataset">
+                                                <section className="section">
+
+                                                </section>
+                                            </AnterosCard>
+                                        </div>
+
+                                        <div className="col-md-6">
+                                            <AnterosCard caption="With dataset 2">
+                                                <section className="section">
+
+                                                </section>
+                                            </AnterosCard>
+                                        </div>
+                                    </div>
+
+                                </section>
+                            </AnterosCard>
+                        </div>
+                    </div>
+
+
                 </AnterosCard >
             </div>);
     }
